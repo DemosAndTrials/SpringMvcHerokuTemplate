212576e795efc9876e46d2a6999cfecadd057db9
Ros Haitovich <matrostik@gmail.com>
2016-07-23 22:52:41 +0300

    added bootstrap

--- src/main/java/com/sfdc/config/WebConfig.java
+++ src/main/java/com/sfdc/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -21,5 +22,10 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         resolver.setPrefix("/");
         resolver.setSuffix(".jsp");
         return resolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
     }
 }
--- src/main/java/com/sfdc/controller/HomeController.java
+++ src/main/java/com/sfdc/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.sfdc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Calendar;
 
 /**
  * Created by Ros on 23/07/2016.
@@ -10,7 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping(value = "/")
-    public String home() {
+    public String home(Model model) {
+        //System.out.println("HomeController");
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        model.addAttribute("year", year);
 
         return "home";
     }
