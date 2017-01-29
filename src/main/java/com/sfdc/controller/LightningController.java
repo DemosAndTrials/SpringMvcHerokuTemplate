91c1b882770237f56cf289d020ea8ebd3d87e731
Ros Haitovich <matrostik@gmail.com>
2017-01-29 12:07:47 +0200

    Deploy

--- src/main/java/com/sfdc/controller/LightningController.java
+++ src/main/java/com/sfdc/controller/LightningController.java
@@ -1,6 +1,6 @@
 package com.sfdc.controller;
 
-import jdk.nashorn.internal.runtime.Debug;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
